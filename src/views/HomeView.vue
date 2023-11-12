66af725e9cef4f9d6eeae7effc0c3ee1b5f54509
晓龙 <280751922@qq.com>
2023-11-12 20:23:00 +0800

    'init'

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-xl text-pink-400">Vue 全家桶 + Vant 移动端项目模板</h1>
+  <ul>
+    <li>Vue 3.0</li>
+    <li>Vue Router 4.0</li>
+    <li>Pinia</li>
+    <li>Vant</li>
+    <li>Tailwindcss</li>
+    <li>Vite</li>
+    <li>Axios</li>
+    <li>postcss-px-to-viewport</li>
+    <li>pinia-plugin-persistedstate</li>
+  </ul>
+</template>
